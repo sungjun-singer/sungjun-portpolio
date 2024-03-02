4790d6d988c208749681a80bd71f32dde84a5ea2
“sungjunkim” <kis07150@naver.com>
2024-03-02 10:54:38 +0900

    메인 내용 수정

--- components/home/hero.js
+++ components/home/hero.js
@@ -8,7 +8,7 @@ export default function Hero(){
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">안녕하세요 김성준입니다.
                 <br className="hidden lg:inline-block"/>오늘도 빡코딩!
                 </h1>
-                <p className="mb-8 leading-relaxed">대한민국의 경제질서는 개인과 기업의 경제상의 자유와 창의를 존중함을 기본으로 한다. 탄핵결정은 공직으로부터 파면함에 그친다. 그러나, 이에 의하여 민사상이나 형사상의 책임이 면제되지는 아니한다.</p>
+                <p className="mb-8 leading-relaxed">포트폴리오 완성! 디자인 수정과 내용 수정해줘야한다 이제~</p>
                 <div className="flex justify-center">
                 <Link href="/projects">
                     <span 
